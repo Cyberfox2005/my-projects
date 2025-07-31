60bfc7686e3296684ba4ab9dc2144cb59aaac1cc
Benachi Abd elouaheb <ben689533@gmail.com>
2025-07-31 20:50:33 +0100

    transportation project

---
+++ index.js
@@ -0,0 +1,7 @@
+let age=20;
+let name="godcode";
+let isMarried=false;
+document.getilementbyId("name").innerHTML=name;
+document.getilementbyId(age)
+document.getElementById(isMarried).innerHTML=isMarried;
+
